f23fa33c43673dbdf3d9b3e7056760749bd33322
Emili Kaiman <emilikaiman@gmail.com>
2021-11-12 11:19:26 -0700

    Complete second object assignment

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var townhouse = {
+  neighborhood: 'Fox',
+  streetLocated: 'Huron',
+  houseNumber: 4389,
+  zipCode: 80110,
+  hasGarage: true,
+  hasYard: false,
+  houseFeatures: ['new appliances', '2 car garage', '4 bedrooms'],
+  houseFlaws: ['leaky sink', 'bad roof', 'creaky floors']
+}
