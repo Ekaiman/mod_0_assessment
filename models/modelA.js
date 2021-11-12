2b2525bedd2010bcf1cd5dd5220d3b821b245ffc
Emili Kaiman <emilikaiman@gmail.com>
2021-11-12 11:07:20 -0700

    Complete first object assignment

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var laptop = {
+  numberOfKeys: 150,
+  yearMade: 2017,
+  screenWidth: 13.5,
+  screenHeight: 8.4,
+  isNew: false,
+  isWorking: true,
+  features: ['stainless steel', 'large screen'],
+  uses: ['access internet', 'watch netflix']
+};
