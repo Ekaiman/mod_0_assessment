a9fa8b78d86f4a12db87c4b9f9feb69ee098ca3d
Emili Kaiman <emilikaiman@gmail.com>
2021-11-12 10:58:43 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,10 @@
+var animal = 'Dog';
+var isCuddly = true;
+animal = 'Cat';
+var num8 = 88
+var num3 = 33
+var quotient = num3 / num8;
+var favoriteFilms = ['Forrest Gump', 'Die Hard', 'The Notebook']
+favoriteFilms.shift();
+console.log(favoriteFilms);
+console.log(animal.length);
